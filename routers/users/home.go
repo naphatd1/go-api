98eba80868dc88b28d4c500d96d39a4a09bda07c
Naphat <naphat.d@gmail.com>
2024-02-25 18:58:33 +0700

    Go API/users/register

--- routers/users/home.go
+++ routers/users/home.go
@@ -4,9 +4,16 @@ import "github.com/gin-gonic/gin"
 
 func InitUsersRoutes(rg *gin.RouterGroup) {
 	routerGroup := rg.Group("/users")
+
 	routerGroup.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"data": "users",
+		})
+	})
+
+	routerGroup.POST("/register", func(c *gin.Context) {
+		c.JSON(201, gin.H{
+			"data": "register",
 		})
 	})
 }
